ecb7bab77314cdeee02022d9b48f732171ff4be7
Andrey Butenko <whitespysoftware@gmail.com>
2019-03-17 17:38:28 +0200

    Bumped version to 0.9.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-node-assets',
-    version='0.9.3',
+    version='0.9.4',
     description='The Django application that allows install and serve assets via Node.js package '
                 'manager infrastructure.',
     author='Andrey Butenko',
@@ -12,5 +12,22 @@ setup(
     long_description=open('README.rst').read(),
     packages=find_packages(),
     include_package_data=True,
-    keywords='django assets staticfiles Node.js npm'
+    keywords='django assets staticfiles Node.js npm',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
 )
