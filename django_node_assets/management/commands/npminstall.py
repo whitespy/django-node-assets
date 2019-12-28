619e399650a94182ebc0bc375bab9a51f88b391a
Andrey Butenko <whitespysoftware@gmail.com>
2019-12-28 21:14:30 +0200

    Stopped using symbolic links to copy package.json

--- django_node_assets/management/commands/npminstall.py
+++ django_node_assets/management/commands/npminstall.py
@@ -1,22 +1,27 @@
 import os
 import os.path
-from contextlib import contextmanager
+import shutil
 from subprocess import Popen, PIPE, STDOUT
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-NODE_PACKAGE_DIR = os.path.dirname(settings.NODE_MODULES_ROOT)
 
+class NodePackageContext:
 
-@contextmanager
-def node_package_context():
-    package_json_symlink = os.path.join(NODE_PACKAGE_DIR, 'package.json')
-    if package_json_symlink != settings.NODE_PACKAGE_JSON:
-        os.symlink(settings.NODE_PACKAGE_JSON, package_json_symlink)
-    yield
-    if os.path.exists(package_json_symlink) and os.path.islink(package_json_symlink):
-        os.unlink(package_json_symlink)
+    def __init__(self):
+        self.package_dir = os.path.dirname(settings.NODE_MODULES_ROOT)
+        self.package_json = os.path.join(self.package_dir, 'package.json')
+
+    def __enter__(self):
+        if not os.path.samefile(self.package_json, settings.NODE_PACKAGE_JSON):
+            shutil.copy(settings.NODE_PACKAGE_JSON, self.package_json)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not os.path.samefile(self.package_json, settings.NODE_PACKAGE_JSON) and os.path.exists(self.package_json):
+            os.remove(self.package_json)
+        return False
 
 
 class Command(BaseCommand):
@@ -24,14 +29,25 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         if not hasattr(settings, 'NODE_PACKAGE_JSON'):
-            self.stderr.write('The NODE_PACKAGE_JSON option does not specified.')
+            self.stderr.write('The NODE_PACKAGE_JSON option is not specified.')
             return
         if not os.path.exists(settings.NODE_PACKAGE_JSON):
             self.stderr.write('The package.json not found.')
-        with node_package_context():
-            with Popen(args=['install', '--no-package-lock', '--prefix={}'.format(NODE_PACKAGE_DIR)],
-                       executable=getattr(settings, 'NODE_PACKAGE_MANAGER_EXECUTABLE', '/usr/bin/npm'),
-                       shell=True, stdout=PIPE, stderr=STDOUT) as p:
+            return
+        if not os.path.exists(settings.NODE_MODULES_ROOT):
+            self.stderr.write('The {} directory does not exist.'.format(settings.NODE_MODULES_ROOT))
+            return
+        if not os.path.isdir(settings.NODE_MODULES_ROOT):
+            self.stderr.write('The {} is not directory.'.format(settings.NODE_MODULES_ROOT))
+            return
+        with NodePackageContext() as node_package_context:
+            with Popen(
+                    args=['install', '--no-package-lock', '--prefix={}'.format(node_package_context.package_dir)],
+                    executable=getattr(settings, 'NODE_PACKAGE_MANAGER_EXECUTABLE', '/usr/bin/npm'),
+                    shell=True,
+                    stdout=PIPE,
+                    stderr=STDOUT,
+            ) as p:
                 for line in p.stdout:
                     decoded_line = line.decode()
                     if decoded_line.startswith('npm WARN'):
