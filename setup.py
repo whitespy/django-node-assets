a774aa68a5272c9d7de20a07c6fc8b0d2f52f7ab
Andrey Butenko <whitespysoftware@gmail.com>
2023-03-13 01:00:27 +0200

    Declared compatibility with Python 3.11 and Django 4.1

--- setup.py
+++ setup.py
@@ -22,9 +22,11 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
     ],
 )
