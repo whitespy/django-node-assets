56bad22a79de5ee77686e05f9ee33ec57048efed
Andrey Butenko <whitespysoftware@gmail.com>
2023-03-13 01:05:19 +0200

    Bumped version to 0.9.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-node-assets',
-    version='0.9.11',
+    version='0.9.12',
     description='The Django application that allows install and serve assets via Node.js package '
     'manager infrastructure.',
     author='Andrey Butenko',
